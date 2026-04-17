bdf786f6d0005c607cc6b8a600ed1fac25a57ed6
Petter5-5 <yeraytrejosanchez@gmail.com>
2026-04-17 16:38:47 +0200

    Dio favicon

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,17 @@
+const favicons = [
+    "recursos/imagenes/faviconV1.png",
+    "recursos/imagenes/faviconV2.png",
+    "recursos/imagenes/faviconV3.png"
+    ]
+
 document.addEventListener("DOMContentLoaded", function() {
 
     var btnJugar = document.getElementById("btn-jugar");
     var musicaMenu = document.getElementById("musica-menu");
+
+    const favicon = favicons[Math.floor(Math.random() * favicons.length)];
+    const linkFavi = document.getElementById("favicon");
+    linkFavi.href = favicon;
 
     if (musicaMenu) {
         musicaMenu.volume = 0.3;
